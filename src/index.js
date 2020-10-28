7fe170064e9e731ce858379b54e33e83c9dd9327
108276 <108276@acbel.com>
2020-10-28 22:11:48 +0800

    format code & add function HandleFollow

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 const { router, route, text } = require('bottender/router')
 
 async function SayHi(context) {
-  const count = context.state.count + 1
-  context.setState({
-    count
-  })
-  await context.sendText(`Hi! ${count}`);
-  await context.sendText(`context.platform: ${context.platform}`);
-  await context.sendText(`session.id: ${context.session.id}`);
+	const count = context.state.count + 1
+	context.setState({
+		count
+	})
+	await context.sendText(`Hi! ${count}`);
+	await context.sendText(`context.platform: ${context.platform}`);
+	await context.sendText(`session.id: ${context.session.id}`);
 }
 
 async function Unknown(context) {
-  await context.sendText('Sorry. I do not understand what you say.');
+	await context.sendText('不要吵我～');
+}
+
+async function HandleFollow(context) {
+	await context.sendText(`誰准許你加我的(o｀з’*) 你吵到我睡覺了捏！！！`);
 }
 
 module.exports = async function App(context) {
-  return router([
-    text(['hi', 'hello', 'hey'], SayHi),
-    route('*', Unknown)
-  ])
+	if (context.event.isFollow) {
+		return HandleFollow
+	} else if (context.event.isText) {
+		return router([
+			text(['hi', 'hello', 'hey'], SayHi),
+			route('*', Unknown)
+		])
+	} else {
+		return Unknown()
+	}
 }
